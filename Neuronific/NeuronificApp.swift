ee3bb624cc461853e959ce1c63a12177b99eb651
Cameron de Bruyn <boertjie.seun@outlook.com>
2021-04-28 10:53:41 +0200

    Initial Commit

---
+++ Neuronific/NeuronificApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuronificApp.swift
+//  Neuronific
+//
+//  Created by Cameron de Bruyn on 2021/04/28.
+//
+
+import SwiftUI
+
+@main
+struct NeuronificApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
