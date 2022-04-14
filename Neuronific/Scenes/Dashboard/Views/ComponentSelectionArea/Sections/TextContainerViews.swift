d88867a39ec1f0dbbdd00b6ce16bb2dfe5e8f514
Cameron de Bruyn <debruyn.sarel@gmail.com>
2022-04-14 04:32:13 +0200

    Updated init for NSTextStorage string

--- Neuronific/Scenes/Dashboard/Views/ComponentSelectionArea/Sections/TextContainerViews.swift
+++ Neuronific/Scenes/Dashboard/Views/ComponentSelectionArea/Sections/TextContainerViews.swift
@@ -13,7 +13,8 @@ import Parsec
 
 enum TextKitContainers: String, CaseIterable
 {
-    case basic = "NSTextStorage: init()"
+    case textKit_init_1 = "NSTextStorage: init()"
+    case textKit_init_2 = "NSTextStorage: init(string:)"
 }
 
 struct TextKitContainerViews : View
@@ -26,7 +27,7 @@ struct TextKitContainerViews : View
 
             switch row
             {
-            case .basic:
+            case .textKit_init_1:
                 
                 guard let contentURL = Bundle.main.url(
                     forResource:   "textkit_nstextstorage_init_1",
@@ -54,6 +55,37 @@ struct TextKitContainerViews : View
                         Implemented by subclasses to initialise a new object
                         (the receiver) immediately after memory has been
                         allocated for it.
+                    """,
+                    content: content
+                )
+                
+            case .textKit_init_2:
+                
+                guard let contentURL = Bundle.main.url(
+                    forResource:   "textkit_nstextstorage_init_2",
+                    withExtension: "json"
+                )
+                else
+                {
+                    debugPrint("ERROR: Failed to fetch `textkit_nstextstorage_init_2.json` from Bundle.main")
+                    
+                    return nil
+                }
+                
+                guard let contentData = try? Data(contentsOf: contentURL),
+                      let content     = String(data: contentData, encoding: .utf8)
+                else
+                {
+                    debugPrint("ERROR: Failed to read data from `textkit_nstextstorage_init_2.json`")
+                    
+                    return nil
+                }
+                
+                return TitledPopoverCardView(
+                    title: row.rawValue,
+                    type: """
+                        Creates an attributedString with the specified
+                        string and attributes.
                     """,
                     content: content
                 )
